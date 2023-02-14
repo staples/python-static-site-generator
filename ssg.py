502ec8154f9e60d8700f9de9838dc4eb502b5696
Wesley Denaro <wesley.denaro@staples.com>
2023-02-14 17:20:58 -0500

    Committing 3.1 - 3.14

--- ssg.py
+++ ssg.py
@@ -1,10 +1,15 @@
 import typer
 
+import ssg.parsers
 from ssg.site import Site
+import ssr.parsers
 
-
-def main(source="content", dest="dist"):
-    config = {"source": source, "dest": dest}
+def main(source='content', dest='dest'):
+    config = {
+        'source': source,
+        'dest': dest,
+        'parsers': [ssg.parsers.ResourceParser(), ],
+    }
 
     Site(**config).build()
 
